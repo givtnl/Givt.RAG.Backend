da0695666e3f2b6a6aaebd853017a399a9e2f408
Anthony <svansay@pansay.com>
2021-04-20 10:28:06 +0200

    add application settings

---
+++ src/backend.business/Infrastructure/ApplicationSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace backend.business.Infrastructure
+{
+    public class ApplicationSettings
+    {
+        public string TablePrefix { get; set; }
+        public string ParticipantFinishedQueue { get; set; }
+    }
+}
